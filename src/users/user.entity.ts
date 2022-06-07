0136150efafc516e21f959458fccde408f6f9dfe
iceberg <geolrco124@hotmail.com>
2022-06-07 01:55:08 -0500

    feat: Added Layers components

--- src/users/user.entity.ts
+++ src/users/user.entity.ts
@@ -1,8 +1,20 @@
-import { Entity } from 'typeorm';
+import { Column, Entity, PrimaryGeneratedColumn} from 'typeorm';
 
 @Entity('users')
 export class UserEntity {
+  @PrimaryGeneratedColumn()
   id: number;
+
+  @Column()
   name: string;
+
+  @Column()
   email: string;
+
+  hashPassword(){
+    //this.password=crypto.createHmac('sha256', this.password).digest('hex');
+  }
+
+  @Column()
+  password: string;
 }
